527e324f8319df145263cda76cbafd4f4f985e96
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-01-15 22:01:51 +0100

    Accessing ip anc mac addresses

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use sysinfo::Networks;
+use std::net::IpAddr;
 
 /// Application that prints your network interfaces with associated information, such as ipv4 address, status etc
 #[derive(Parser, Debug)]
@@ -24,15 +25,30 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    get_interfaces(args);
+}
 
+fn get_interfaces(args: Args) {
     if args.mac {
         println!("Mac argument is set!");
     }
 
     let networks = Networks::new_with_refreshed_list();
-    for (interface_name, network) in &networks {
-        //println!("[{interface_name}]: {network:?}");
-        println!("{interface_name}");
-        //network.ip_networks()
+    for (interface_name, data) in &networks {
+        println!("Interface: {interface_name}");
+        let ip_networks = data.ip_networks();
+        for ip_network in ip_networks {
+            let addr = &ip_network.addr;
+            match addr {
+                IpAddr::V4(ipv4) => {
+                    println!("IPv4: {}", ipv4);
+                }
+                IpAddr::V6(ipv6) => {
+                    println!("IPv6: {}", ipv6);
+                }
+            }
+        }
+        let mac_addr = data.mac_address();
+        println!("mac addr: {mac_addr}");
     }
 }
