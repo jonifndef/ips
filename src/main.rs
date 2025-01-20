96f988107cc9a636502e66e7cdc92817f51bf71f
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-01-20 09:17:05 +0100

    Change again, to pnet crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-use nix::ifaddrs;
+//use nix::ifaddrs;
+use pnet::datalink::{self, NetworkInterface};
+use pnet::ipnetwork::IpNetwork;
 
 // Use this (nix crate), or MAAAYBE pnet crate?
 
@@ -24,6 +26,16 @@ struct Args {
     connections: bool
 }
 
+struct OutputFields {
+    interface_name: String,
+    ip_addr: String,
+    mac_addr: String,
+    status: String,
+    ipv6_addr: String,
+    gateway: String,
+    connections: String
+}
+
 fn main() {
     let args = Args::parse();
     get_interfaces(args);
@@ -34,16 +46,35 @@ fn get_interfaces(args: Args) {
         println!("Mac argument is set!");
     }
 
-    let if_addrs = match ifaddrs::getifaddrs() {
-        Ok(addrs) => addrs,
-        Err(e) => {
-            println!("Could not get network interfaces: {e}");
-            return;
+    let interfaces = pnet::datalink::interfaces();
+    for interface in interfaces {
+        if interface.is_loopback() {
+            continue;
         }
-    };
+        if interface.is_up() {
+            println!("its up");
+        }
+        println!("interface name is: {}", interface.name);
+        for ip in interface.ips {
+            match ip {
+                IpNetwork::V4(ip_addr) => { println!("Ipv4 addr: {}", ip_addr) }
+                IpNetwork::V6(ip_addr) => { println!("Ipv6 addr: {}", ip_addr) }
+            }
+        }
 
-    for if_addr in if_addrs {
-        //println!("{:?}, {:?}, {:?}, {:?}", if_addr.interface_name, if_addr.address, if_addr.netmask, if_addr.flags);
-        println!("{}", if_addr.interface_name);
+        println!("{:?}", interface.mac);
     }
+
+    //let if_addrs = match ifaddrs::getifaddrs() {
+    //    Ok(addrs) => addrs,
+    //    Err(e) => {
+    //        println!("Could not get network interfaces: {e}");
+    //        return;
+    //    }
+    //};
+
+    //for if_addr in if_addrs {
+    //    //println!("{:?}, {:?}, {:?}, {:?}", if_addr.interface_name, if_addr.address, if_addr.netmask, if_addr.flags);
+    //    println!("{}", if_addr.interface_name);
+    //}
 }
