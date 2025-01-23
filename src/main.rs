0c30daef3c018681bdba7b24299d31dd54f6e4b0
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-01-23 12:41:31 +0100

    Print connection info, simplest case

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,6 @@ use pnet::datalink;
 use pnet::ipnetwork::IpNetwork;
 use std::process::{Command, Stdio};
 
-// to get gateway, use something like:
-// route -n | grep 'UG[ \t]' | awk '{print $2}'
-// or:
-// route -n | grep 'UG[ \t]' | grep 'wlp2s0' | awk '{print $2}'
-// to get connections that use this interface, run:
-// nmcli -t con show | grep "wlp2s0", and parse the first field
-// nmcli -t con show | grep "wlp2s0" | awk -F: '{print $1}'
-
 /// Application that prints your network interfaces with associated information, such as ipv4 address, status etc
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -76,62 +68,114 @@ fn get_interfaces(args: Args) {
             println!("its up");
         }
 
-        // route -n | grep 'UG[ \t]' | grep 'wlp2s0' | awk '{print $2}'
-        let route_child = match Command::new("route")
-            .arg("-n")
-            .stdout(Stdio::piped())
-            .spawn() {
-            Ok(route_child) => route_child,
-            Err(_) => { return; }
-        };
+        get_gateway(&interface);
+        get_connections(&interface);
+    }
+}
 
-        let route_out = match route_child.stdout {
-            Some(route_out) => route_out,
-            None => { println!("None route_out"); return; }
-        };
+fn get_gateway(interface: &datalink::NetworkInterface) {
+    // route -n | grep 'UG[ \t]' | grep 'wlp2s0' | awk '{print $2}'
+    let route_child = match Command::new("route")
+        .arg("-n")
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(route_child) => route_child,
+        Err(_) => { return; }
+    };
 
-        let gw_grep_child = match Command::new("grep")
-            .arg("UG[ \\t]")
-            .stdin(Stdio::from(route_out))
-            .stdout(Stdio::piped())
-            .spawn() {
-            Ok(gw_grep_child) => gw_grep_child,
-            Err(_) => { return; }
-        };
+    let route_out = match route_child.stdout {
+        Some(route_out) => route_out,
+        None => { println!("None route_out"); return; }
+    };
 
-        let gw_grep_out = match gw_grep_child.stdout {
-            Some(gw_grep_out) => gw_grep_out,
-            None => { println!("None route_out"); return; }
-        };
+    let gw_grep_child = match Command::new("grep")
+        .arg("UG[ \\t]")
+        .stdin(Stdio::from(route_out))
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(gw_grep_child) => gw_grep_child,
+        Err(_) => { return; }
+    };
 
-        let ifc_grep_child = match Command::new("grep")
-            .arg(interface.name)
-            .stdin(Stdio::from(gw_grep_out))
-            .stdout(Stdio::piped())
-            .spawn() {
-            Ok(ifc_grep_child) => ifc_grep_child,
-            Err(_) => { return; }
-        };
+    let gw_grep_out = match gw_grep_child.stdout {
+        Some(gw_grep_out) => gw_grep_out,
+        None => { println!("None route_out"); return; }
+    };
 
-        let ifc_grep_out = match ifc_grep_child.stdout {
-            Some(ifc_grep_out) => ifc_grep_out,
-            None => { println!("None route_out"); return; }
-        };
+    let ifc_grep_child = match Command::new("grep")
+        .arg(&interface.name)
+        .stdin(Stdio::from(gw_grep_out))
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(ifc_grep_child) => ifc_grep_child,
+        Err(_) => { return; }
+    };
 
-        let awk_child = match Command::new("awk")
-            .arg("{print $2}")
-            .stdin(Stdio::from(ifc_grep_out))
-            .stdout(Stdio::piped())
-            .spawn() {
-            Ok(awk_child) => awk_child,
-            Err(_) => { return; }
-        };
+    let ifc_grep_out = match ifc_grep_child.stdout {
+        Some(ifc_grep_out) => ifc_grep_out,
+        None => { println!("None route_out"); return; }
+    };
+
+    let awk_child = match Command::new("awk")
+        .arg("{print $2}")
+        .stdin(Stdio::from(ifc_grep_out))
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(awk_child) => awk_child,
+        Err(_) => { return; }
+    };
 
 
-        let output = awk_child.wait_with_output().expect("Failed to wait on sed");
-        //let output_str = String::from_utf8(output.stdout);
-        if let Ok(out_str) = String::from_utf8(output.stdout) {
-            println!("Output: {}", out_str);
-        }
+    let output = awk_child.wait_with_output().expect("Failed to wait on sed");
+
+    if let Ok(out_str) = String::from_utf8(output.stdout) {
+        let trimmed_out_str = out_str.trim_end();
+        println!("Output from gateway: {}", trimmed_out_str);
+    }
+}
+
+fn get_connections(interface: &datalink::NetworkInterface) {
+    // nmcli -t con show | grep "wlp2s0" | awk -F: '{print $1}'
+    let nmcli_child = match Command::new("nmcli")
+        .args(["-t", "con", "show"])
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(nmcli_child) => nmcli_child,
+        Err(_) => { return; }
+    };
+
+    let nmcli_out = match nmcli_child.stdout {
+        Some(nmcli_out) => nmcli_out,
+        None => { println!("None nmcli_out"); return; }
+    };
+
+    let grep_child = match Command::new("grep")
+        .arg(&interface.name)
+        .stdin(Stdio::from(nmcli_out))
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(grep_child) => grep_child,
+        Err(_) => { return; }
+    };
+
+    let grep_out = match grep_child.stdout {
+        Some(grep_out) => grep_out,
+        None => { println!("None grep_out"); return; }
+    };
+
+    let awk_child = match Command::new("awk")
+        .args(["-F:", "{print $1}"])
+        .stdin(Stdio::from(grep_out))
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(awk_child) => awk_child,
+        Err(_) => { return; }
+    };
+
+    let output = awk_child.wait_with_output().expect("Failed to wait on sed");
+
+    if let Ok(out_str) = String::from_utf8(output.stdout) {
+        let trimmed_out_str = out_str.trim_end();
+        println!("Output from connections: {}", trimmed_out_str);
     }
 }
