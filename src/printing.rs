a3ecc2c380f64344c66e299a720b7d53635387d6
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-02-09 13:08:43 +0100

    WiP print ipv6 - add second addr on new line

--- src/printing.rs
+++ src/printing.rs
@@ -26,44 +26,67 @@ impl ColorTokens {
 
 pub fn print_interfaces(args: crate::Args, interfaces: Vec<interface_data::InterfaceData>) {
     let widths = get_output_widths(&interfaces);
+    let mut lines: Vec<String> = vec![];
 
     for interface in interfaces {
-        let mut line = format!("{:<name_width$} {:<ip_width$} {:<status_width$}",
+        let i = lines.len();
+        let line = format!("{:<name_width$} {:<ip_width$} {:<status_width$}",
             colorize_string_if_enabled(&interface.interface_name, args.nocolor, ColorTokens::GREEN),
             colorize_string_if_enabled(&interface.ip_addr, args.nocolor, ColorTokens::YELLOW),
             colorize_string_if_enabled(&interface.status, args.nocolor, ColorTokens::BRIGHT_GREEN),
             name_width = widths.interface_name + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN },
             ip_width = widths.ip_addr + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN },
             status_width = widths.status + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
+        lines.push(line);
 
         if args.mac {
             let mac = format!(" {:<mac_width$}",
                 colorize_string_if_enabled(&interface.mac_addr, args.nocolor, ColorTokens::RED),
                 mac_width = widths.mac + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
-            line.push_str(&mac);
+            if let Some(line) = lines.get_mut(i) {
+                line.push_str(&mac);
+            }
         }
 
-        if args.ipv6 && !interface.ipv6_addrs.is_empty() {
-            let ipv6 = format!(" {:<ipv6_width$}",
-                colorize_string_if_enabled(&interface.ipv6_addrs[0], args.nocolor, ColorTokens::BLUE),
-                ipv6_width = widths.ipv6 + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
-                line.push_str(&ipv6);
+        if args.ipv6 {
+            let mut ipv6_idx = i;
+            for addr in interface.ipv6_addrs.iter() {
+                let ipv6 = format!(" {:<ipv6_width$}",
+                    colorize_string_if_enabled(addr, args.nocolor, ColorTokens::BLUE),
+                    ipv6_width = widths.ipv6 + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
+                if let Some(line) = lines.get_mut(ipv6_idx) {
+                    line.push_str(&ipv6);
+                    ipv6_idx += 1;
+                } else {
+                    let ipv6 = format!(" {:<ipv6_width$}",
+                        colorize_string_if_enabled(addr, args.nocolor, ColorTokens::BLUE),
+                        ipv6_width = widths.ipv6 + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
+                    lines.push(ipv6);
+                    ipv6_idx += 1;
+                }
+            }
         }
 
         if args.gateway {
             let gateway = format!(" {:<gateway_width$}",
                 colorize_string_if_enabled(&interface.gateway, args.nocolor, ColorTokens::BLUE),
                 gateway_width = widths.gateway + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
-            line.push_str(&gateway);
+            if let Some(line) = lines.get_mut(i) {
+                line.push_str(&gateway);
+            }
         }
 
         if args.connections && !interface.connections.is_empty() {
             let connection = format!(" {:<connection_width$}",
                 colorize_string_if_enabled(&interface.connections[0], args.nocolor, ColorTokens::BLUE),
                 connection_width = widths.connections + if args.nocolor { 0 } else { ColorTokens::TOKENS_LEN });
+            if let Some(line) = lines.get_mut(i) {
                 line.push_str(&connection);
+            }
         }
+    }
 
+    for line in lines {
         println!("{line}");
     }
 }
